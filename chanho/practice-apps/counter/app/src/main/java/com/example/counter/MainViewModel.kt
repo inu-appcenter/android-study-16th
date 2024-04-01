24c84a64793f506dfedb98cacb788cd1afbce743
ACH1002 <anchanho1002@gmail.com>
2024-04-01 21:15:33 +0900

    Add counter app
    2주차 연습용 카운터 앱

---
+++ chanho/practice-apps/counter/app/src/main/java/com/example/counter/MainViewModel.kt
@@ -0,0 +1,23 @@
+package com.example.counter
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+
+    private val _counter = MutableLiveData<Int>()
+    val counter : LiveData<Int>
+        get() = _counter
+
+    init {
+        _counter.value = 0
+    }
+    fun plusCount(){
+        _counter.value = counter.value?.plus(1)
+    }
+
+    fun minusCount(){
+        _counter.value = counter.value?.minus(1)
+    }
+}
